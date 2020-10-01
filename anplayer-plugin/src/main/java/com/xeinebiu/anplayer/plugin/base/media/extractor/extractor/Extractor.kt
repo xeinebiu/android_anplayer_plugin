22dbf18779ce8872751160fe89d05905c1e29d56
xeinebiu <xeinebiu@gmail.com>
2020-10-01 12:14:02 +0200

    feature(anplayer-plugin) - Extractor.kt implement Iterator

--- anplayer-plugin/src/main/java/com/xeinebiu/anplayer/plugin/base/media/extractor/extractor/Extractor.kt
+++ anplayer-plugin/src/main/java/com/xeinebiu/anplayer/plugin/base/media/extractor/extractor/Extractor.kt
@@ -1,18 +1,8 @@
 package com.xeinebiu.anplayer.plugin.base.media.extractor.extractor
 
-abstract class Extractor<T>(val extractorCode: String) {
+abstract class Extractor<out T>(val extractorCode: String) : Iterator<List<T>> {
     /**
      * Reset the cursor
      */
     abstract fun reset()
-
-    /**
-     * Returns next page content
-     */
-    abstract fun next(): List<T>
-
-    /**
-     * Returns `true` if there is a next page available, `false` otherwise
-     */
-    abstract fun hasNext(): Boolean
 }
