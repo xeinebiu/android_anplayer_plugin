0a569937bc2e3fb6e77b30b3cde13a3d32c1974c
Arzan Nebiu <xeinebiu@gmail.com>
2021-10-26 23:52:45 +0200

    feaute(1.53.0) - extend UserInputNeededException.kt with a "isResolved" callback & remove cookieManager

--- anplayer-plugin/src/main/java/com/xeinebiu/anplayer/plugin/media/extractor/exception/UserInputNeededException.kt
+++ anplayer-plugin/src/main/java/com/xeinebiu/anplayer/plugin/media/extractor/exception/UserInputNeededException.kt
@@ -1,17 +1,18 @@
 package com.xeinebiu.anplayer.plugin.media.extractor.exception
 
 import android.net.Uri
-import java.net.CookieManager
 
 /**
  * Exception to throw when a Captcha or any user feedback is required
  *
  * @param uri - The uri to open in browser
- * @param cookieManager - Read & Write cookies to
+ * @param userAgent - User agent to set in browser
+ * @param isResolved - Validate if issue is resolved
  * @param cause - The exception if any
  */
 class UserInputNeededException(
     val uri: Uri,
-    val cookieManager: CookieManager,
+    val userAgent: String,
+    val isResolved: (html: String) -> Boolean,
     cause: Throwable?
 ) : MediaExtractorException(uri.toString(), cause)
