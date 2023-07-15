86e9b7488daf98b7f857b779983264ebfbee0569
xeinebiu <xeinebiu@gmail.com>
2023-07-16 00:24:37 +0200

    remove URL dependency from getCategroy api

--- packages/plugin/src/lib/extractor/category.extractor.ts
+++ packages/plugin/src/lib/extractor/category.extractor.ts
@@ -3,5 +3,5 @@ import { Album, AlbumDetail, Category, Media, MediaDetail } from '../model';
 
 export type CategoryExtractor = Extractor<
     Media | Album | MediaDetail | AlbumDetail,
-    { getCategory(uri: URL): Promise<Category> }
+    { getCategory(): Promise<Category> }
 >;
