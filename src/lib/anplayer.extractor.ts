d8a0ab74f2499c6215848812362d9f379ab7fa68
xeinebiu <xeinebiu@gmail.com>
2025-02-03 20:00:15 +0100

    feature: add utility function to easily create extractors

--- src/lib/anplayer.extractor.ts
+++ src/lib/anplayer.extractor.ts
@@ -13,6 +13,7 @@ import {
     StreamExtractor,
 } from './extractor';
 import { ExtractorLogger } from './logger';
+import { ExtractorNotImplementedError } from './error';
 
 export type AnPlayerExtractor = (args: {
     temporaryFolderPath: string;
@@ -59,3 +60,69 @@ export type AnPlayerExtractor = (args: {
 
     isSupported(uri: URL): Promise<boolean>;
 };
+
+const defaultImplementation = {
+    getAccountExtractor: () =>
+        Promise.reject(new ExtractorNotImplementedError()),
+    getAlbumExtractor: () => Promise.reject(new ExtractorNotImplementedError()),
+    getAlbumsExtractor: () =>
+        Promise.reject(new ExtractorNotImplementedError()),
+    getAuthorExtractor: () =>
+        Promise.reject(new ExtractorNotImplementedError()),
+    getAuthorsExtractor: () =>
+        Promise.reject(new ExtractorNotImplementedError()),
+    getAutocompleteExtractor: () =>
+        Promise.reject(new ExtractorNotImplementedError()),
+    getCategoriesExtractor: () =>
+        Promise.reject(new ExtractorNotImplementedError()),
+    getCategoryExtractor: () =>
+        Promise.reject(new ExtractorNotImplementedError()),
+    getFeedExtractor: () => Promise.reject(new ExtractorNotImplementedError()),
+    getHeaders: () => Promise.reject(new ExtractorNotImplementedError()),
+    getMediaExtractor: () => Promise.reject(new ExtractorNotImplementedError()),
+    getSearchExtractor: () =>
+        Promise.reject(new ExtractorNotImplementedError()),
+    getStreamExtractor: () =>
+        Promise.reject(new ExtractorNotImplementedError()),
+    isAlbum: () => Promise.resolve(false),
+    isAuthor: () => Promise.resolve(false),
+    isCategory: () => Promise.resolve(false),
+    isMediaTrack: () => Promise.resolve(false),
+    isDirectStream: () => Promise.resolve(false),
+    isDescriptionStream: () => Promise.resolve(false),
+    isSupported: () => Promise.resolve(false),
+};
+
+export function createAnPlayerExtractor(
+    partialImplementation: Partial<AnPlayerExtractor>,
+): AnPlayerExtractor {
+    return () => {
+        const implementation = {
+            ...defaultImplementation,
+            ...partialImplementation,
+        };
+
+        return {
+            getAccountExtractor: implementation.getAccountExtractor,
+            getAlbumExtractor: implementation.getAlbumExtractor,
+            getAlbumsExtractor: implementation.getAlbumsExtractor,
+            getAuthorExtractor: implementation.getAuthorExtractor,
+            getAuthorsExtractor: implementation.getAuthorsExtractor,
+            getAutocompleteExtractor: implementation.getAutocompleteExtractor,
+            getCategoriesExtractor: implementation.getCategoriesExtractor,
+            getCategoryExtractor: implementation.getCategoryExtractor,
+            getFeedExtractor: implementation.getFeedExtractor,
+            getHeaders: implementation.getHeaders,
+            getMediaExtractor: implementation.getMediaExtractor,
+            getSearchExtractor: implementation.getSearchExtractor,
+            getStreamExtractor: implementation.getStreamExtractor,
+            isAlbum: implementation.isAlbum,
+            isAuthor: implementation.isAuthor,
+            isCategory: implementation.isCategory,
+            isMediaTrack: implementation.isMediaTrack,
+            isDirectStream: implementation.isDirectStream,
+            isDescriptionStream: implementation.isDescriptionStream,
+            isSupported: implementation.isSupported,
+        };
+    };
+}
